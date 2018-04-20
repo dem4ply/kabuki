84a6eccd82f12ecb5a2cced33e803de04608c7f4
dem4ply <dem4ply@gmail.com>
2018-04-20 16:03:11 -0500

    se agrega el behavior of seek

---
+++ Assets/_script/controller/2d/AI/behaviour/Ai_seek.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace controller
+{
+	namespace ai
+	{
+		public class Ai_seek : Ai_walk
+		{
+			public GameObject target;
+
+			protected override void Update()
+			{
+				desire_direction = target.transform.position
+					- controller.transform.position;
+				desire_direction.Normalize();
+				base.Update();
+			}
+		}
+	}
+}
