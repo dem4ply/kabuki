3a82028d16c090d81592b617e01508911dacb76c
dem4ply <dem4ply@gmail.com>
2018-09-28 04:33:09 -0500

    se agrego controles para side scroll en 3d

---
+++ Assets/_script/controller/3d/side_scroll/NPC_side_scroll_motor_3d.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace controller
+{
+	namespace motor
+	{
+		public class NPC_side_scroll_motor_3d : NPC_motor_3d
+		{
+			protected override void _proccess_ground_velocity(
+				ref Vector3 velocity_vector )
+			{
+				Vector3 desire_speed_vector;
+				// si es true se esta moviendo a toda velocidad en diagonal
+				if ( direction_vector.magnitude > 1 )
+					desire_speed_vector = direction_vector.normalized * max_speed;
+				else
+					desire_speed_vector = direction_vector * max_speed;
+
+				if ( is_running )
+					desire_speed_vector *= runner_multiply;
+
+				// suavizado de la velocidad horizontal
+				float final_horizontal_velocity = Mathf.SmoothDamp(
+					velocity_vector.z, desire_speed_vector.x,
+					ref horizontal_velocity_smooth, acceleration_time_in_ground );
+
+				velocity_vector.x = final_horizontal_velocity;
+			}
+		}
+	}
+}
+
---
+++ Assets/_tests/scripts/controller/3d/NPC/side_scroll/Test_basic_move.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using controller.motor;
+
+namespace controller
+{
+	namespace motor
+	{
+		namespace tree_d
+		{
+			namespace side_scroll
+			{
+			public class Test_basic_move
+			{
+					GameObject player, scene;
+					tests_tool.Assert_colision left, right;
+
+					[SetUp]
+					public void Instanciate_scenary()
+					{
+						scene =
+							Resources.Load(
+								"_test/scene/controller/3d/side_scroll/basic_move" ) as GameObject;
+						scene = helper.instantiate._( scene );
+						player = scene.transform.Find( "player" ).gameObject;
+						left = scene.transform.Find( "assert_collision_left" )
+							.GetComponent<tests_tool.Assert_colision>();
+						right = scene.transform.Find( "assert_collision_right" )
+							.GetComponent<tests_tool.Assert_colision>();
+					}
+
+					[TearDown]
+					public void clean_scenary()
+					{
+						MonoBehaviour.DestroyImmediate( scene );
+					}
+
+					[UnityTest]
+					public IEnumerator when_the_direction_is_left_shold_touch_left()
+					{
+						var ai = player.GetComponent<ai.Ai_walk>();
+						ai.desire_direction = Vector2.left;
+						yield return new WaitForSeconds( 1 );
+						left.assert_collision_enter( ai.gameObject );
+						right.assert_not_collision_enter();
+					}
+
+					[UnityTest]
+					public IEnumerator when_the_direction_is_right_should_touch_right()
+					{
+						var ai = player.GetComponent<ai.Ai_walk>();
+						ai.desire_direction = Vector2.right;
+						yield return new WaitForSeconds( 1 );
+						right.assert_collision_enter( ai.gameObject );
+						left.assert_not_collision_enter();
+					}
+				}
+
+			}
+		}
+	}
+}
