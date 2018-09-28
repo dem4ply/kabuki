577fe500a2fad74e091143a84949f0043199b4ad
dem4ply <dem4ply@gmail.com>
2018-09-28 11:53:00 -0500

    se agrego el operador de index

--- Assets/_script/snippet/managers/collision_3d.cs
+++ Assets/_script/snippet/managers/collision_3d.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 namespace manager
 {
 	public class Collision {
-		public Dictionary<GameObject, List<Collision_info>> collisions;
+		public Dictionary<
+			GameObject, Dictionary<string, Collision_info>> collisions;
 		public Dictionary<string, List<Collision_info>> collisions_by_name;
 
 		public Collision()
 		{
-			collisions = new Dictionary<GameObject, List< Collision_info>>();
+			collisions = new Dictionary<
+				GameObject, Dictionary<string, Collision_info>>();
 			collisions_by_name = new Dictionary<string, List< Collision_info>>();
 		}
 
@@ -23,22 +25,50 @@ namespace manager
 			throw new NotImplementedException();
 		}
 
+		public Dictionary<string, List< Collision_info>> this[ GameObject obj ]
+		{
+			get{
+				Dictionary<string, List< Collision_info>> result;
+				if ( collisions.TryGetValue( obj, out result ) )
+					return result;
+				else
+					return null;
+			}
+		}
+
+		public Collision_info this[ GameObject obj, string name ]
+		{
+			get{
+				var inner_dict = this[ obj ];
+				if ( inner_dict != null )
+				{
+					Collision_info result;
+					if ( inner_dict.TryGetValue( obj, out result ) )
+						return result;
+				}
+				return null;
+			}
+		}
+
+		public List<Collision_info>this[ string name ]
+		{
+			get{
+				List<Collision_info> result;
+				if ( collisions_by_name.TryGetValue( name, out result ) )
+					return result;
+				else
+					return null;
+			}
+		}
+
 		public List<Collision_info> get( GameObject obj )
 		{
-			List<Collision_info> result;
-			if ( collisions.TryGetValue( obj, out result ) )
-				return result;
-			else
-				return null;
+			return this[ obj ];
 		}
 
 		public List<Collision_info> get( string name )
 		{
-			List<Collision_info> result;
-			if ( collisions_by_name.TryGetValue( name, out result ) )
-				return result;
-			else
-				return null;
+			return this[ name ];
 		}
 	}
 }
