97bf4c9bd2af3248547f4ac2fc60456fdde878ea
dem4ply <dem4ply@gmail.com>
2018-09-28 12:25:16 -0500

    sea grego la funcion para agragar y eliminar collision info

--- Assets/_script/snippet/managers/collision_3d.cs
+++ Assets/_script/snippet/managers/collision_3d.cs
@@ -6,23 +6,52 @@ namespace manager
 	public class Collision {
 		public Dictionary<
 			GameObject, Dictionary<string, Collision_info>> collisions;
-		public Dictionary<string, List<Collision_info>> collisions_by_name;
+		public Dictionary<
+			string, Dictionary<GameObject, Collision_info>> collisions_by_name;
 
 		public Collision()
 		{
 			collisions = new Dictionary<
 				GameObject, Dictionary<string, Collision_info>>();
-			collisions_by_name = new Dictionary<string, List< Collision_info>>();
+			collisions_by_name = new Dictionary<
+				string, Dictionary<GameObject, Collision_info>>();
 		}
 
 		public void add( Collision_info collision_info )
 		{
-			throw new NotImplementedException();
+			var inner_dict = this[ collision_info.game_object ];
+			string name = collision_info.name;
+			GameObject game_object = collision_info.game_object;
+			if ( inner_dict != null )
+				inner_dict[ name ] = collision_info;
+			else
+			{
+				inner_dict = new Dictionary<string, Collision_info>();
+				inner_dict.Add( name, collision_info );
+				collisions.Add( game_object, inner_dict );
+			}
+
+			Dictionary<GameObject, Collision_info> inner_dict_by_name;
+			if ( collisions_by_name.TryGetValue( name, out inner_dict_by_name ) )
+				inner_dict_by_name[ game_object ] = collision_info;
+			else
+			{
+				inner_dict_by_name = new Dictionary<GameObject, Collision_info>();
+				inner_dict_by_name.Add( game_object, collision_info );
+				collisions_by_name.Add( inner_dict_by_name );
+			}
 		}
 
-		public void remove( GameObject obj )
+		public void remove( GameObject game_object )
 		{
-			throw new NotImplementedException();
+			var inner_dict = this[ game_object ];
+			if ( inner_dict == null )
+				return;
+			collisions.Remove( game_object );
+			Dictionary<GameObject, Collision_info> dict_by_name;
+			foreach ( string key in inner_dict.Keys )
+				if ( collisions_by_name.TryGetValue( key, out dict_by_name ) )
+					dict_by_name.Remove( game_object );
 		}
 
 		public Dictionary<string, Collision_info> this[ GameObject obj ]
