1369a8fb8076b74f3126e0ac91c21f02d3c7012c
dem4ply <dem4ply@gmail.com>
2018-10-02 12:22:15 -0500

    se agrego motor para el control de las plataformas

---
+++ Assets/_script/controller/3d/side_scroll/platform_bending/NPC_platform_bending_controller.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+namespace controller
+{
+	namespace controllers
+	{
+		public class NPC_platform_bending_controller
+			: NPC_side_scroll_controller_3d
+		{
+			motor.platform.Platform_motor_base platform_left;
+			motor.platform.Platform_motor_base platform_right;
+		}
+	}
+}
---
+++ Assets/_tests/scripts/controller/3d/platform/motor/summon.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using controller.motor.platform;
+
+namespace controller
+{
+	namespace motor
+	{
+		namespace tree_d
+		{
+			namespace platform
+			{
+				public class Test_summon
+				{
+					GameObject platform, scene, player;
+
+					[SetUp]
+					public void Instanciate_scenary()
+					{
+						scene =
+							Resources.Load(
+								"_test/scene/controller/3d/" +
+								"platform/basic_platform" ) as GameObject;
+						scene = helper.instantiate._( scene );
+						platform = scene.transform.Find( "platform_motor" ).gameObject;
+						player = Resources.Load(
+							"_prefab/player_side_scroll" ) as GameObject;
+						player = helper.instantiate._( player );
+						player.transform.position = new Vector3(
+							platform.transform.position.x,
+							platform.transform.position.y + 3,
+							platform.transform.position.z );
+					}
+
+					[TearDown]
+					public void clean_scenary()
+					{
+						MonoBehaviour.DestroyImmediate( scene );
+					}
+
+					[UnityTest]
+					public IEnumerator should_create_a_new_gameobject()
+					{
+						var motor = platform.GetComponent<Platform_motor_base>();
+						var new_platform = motor.summon();
+						Rigidbody rigidbody = player.GetComponent<Rigidbody>();
+						yield return new WaitForSeconds( 1.5f );
+						Assert.GreaterOrEqual( rigidbody.velocity.y, -0.1f );
+					}
+
+					[UnityTest]
+					public IEnumerator should_spawn_in_the_same_postion_that_motor()
+					{
+						var motor = platform.GetComponent<Platform_motor_base>();
+						var new_platform = motor.summon();
+						yield return new WaitForSeconds( 0.1f );
+						Assert.AreEqual(
+							new_platform.transform.position,
+							motor.transform.position );
+					}
+				}
+			}
+		}
+	}
+}
