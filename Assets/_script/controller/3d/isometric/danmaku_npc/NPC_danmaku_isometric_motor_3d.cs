7436311307946c58740c934a0e39171d909fd924
dem4ply <dem4ply@gmail.com>
2018-11-07 04:32:11 -0600

    se inicia el motor para danmakus

---
+++ Assets/_script/controller/3d/isometric/danmaku_npc/NPC_danmaku_isometric_motor_3d.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace controller
+{
+	namespace isometric
+	{
+		namespace motor
+		{
+			public class NPC_danmaku_isometric_motor_3d
+				: NPC_fly_isometric_motor_3d
+			{
+			}
+		}
+	}
+}
