f8569cafa7873c4b00b3a692a49d232c7f4c540b
dem4ply <dem4ply@gmail.com>
2018-11-11 21:40:40 -0600

    actualziacion

--- Assets/_script/event/scene/Event_scene.cs
+++ Assets/_script/event/scene/Event_scene.cs
@@ -10,10 +10,17 @@ namespace events
 		public class Event_scene : chibi_base.Chibi_behaviour
 		{
 			public GameObject prefab_event_scene;
+			public Transform where_is_goin_open;
 
 			public virtual GameObject open()
 			{
-				GameObject obj = helper.instantiate._( prefab_event_scene );
+				GameObject obj;
+				if ( where_is_goin_open != null )
+					obj = helper.instantiate._(
+						prefab_event_scene, where_is_goin_open.position );
+				else
+					obj = helper.instantiate._(
+						prefab_event_scene, transform.position );
 				return obj;
 			}
 		}
